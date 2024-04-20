18515c1a100715672fc881440ff19d016a0f5254
yangan <yangan@550W-FROM-AN>
2024-04-20 20:37:55 +0800

    实验管理原型发布

---
+++ files/实验运行管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q],r,_(s,t,u,v,g,w,x,_(),y,[],z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,D,L,M,N,null,O,P,Q,R,S,T,U,P,V,W,X,_(F,G,H,Y),Z,P,ba,W,bb,_(bc,bd,be,bf,bg,bf,bh,bf,bi,k,H,_(bj,bk,bl,bk,bm,bk,bn,bo)),i,_(j,k,l,k)),bp,_(),bq,_(),br,_(bs,[])),bt,_(),bu,_());}; 
+var b="url",c="实验运行管理.html",d="generationDate",e=new Date(1713616542440.22),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="page",s="packageId",t="92f9923cd96c464d8c9e2e6ae6d40111",u="type",v="Axure:Page",w="实验运行管理",x="notes",y="annotations",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageAlignment",L="imageRepeat",M="auto",N="favicon",O="sketchFactor",P="0",Q="colorStyle",R="appliedColor",S="fontName",T="Applied Font",U="borderWidth",V="borderVisibility",W="all",X="borderFill",Y=0xFF797979,Z="cornerRadius",ba="cornerVisibility",bb="outerShadow",bc="on",bd=false,be="offsetX",bf=5,bg="offsetY",bh="blurRadius",bi="spread",bj="r",bk=0,bl="g",bm="b",bn="a",bo=0.349019607843137,bp="adaptiveStyles",bq="interactionMap",br="diagram",bs="objects",bt="masters",bu="objectPaths";
+return _creator();
+})());
